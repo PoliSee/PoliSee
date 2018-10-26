d0824c366d4e3a82103e37b16f799043f963465c
Christopher Sabri <csabri2@illinois.edu>
2018-10-26 15:40:53 -0500

    finally got react working a little

--- policy-react/src/index.js
+++ policy-react/src/index.js
@@ -1,29 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.css'
-import {Doughnut} from 'react-chartjs-2';
+import {Line} from 'react-chartjs-2';
 const data = {
 	labels: [
-		'Red',
-		'Green',
-		'Yellow'
+		'7 Days Ago',
+		'6 Days Ago',
+		'5 Days Ago',
+		'4 Days Ago',
+		'3 Days Ago',
+		'2 Days Ago',
+		'1 Day Ago'
 	],
 	datasets: [{
-		data: [300, 50, 100],
-		backgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		],
-		hoverBackgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		]
+			label: 'Canditate 1',
+			fillColor: 'rgba(220,220,220,.2)',
+			strokeColor: 'rgba(220,220,220,1)',
+			pointColor: 'rgba(220,220,220,1)',
+			pointStrokeColor: '#fff',
+			pointHighlightFill: '#fff',
+			pointHighlightStroke: 'rgba(220,220,220,1)',
+			data: [65, 59, 80, 81, 56, 55, 40],
+		},
+		{
+			label: 'Candidate 2',
+			backgroundColor: '#1be55e',
+			borderColor: '#FF0000',
+			pointColor: '#FF0000',
+			pointStrokeColor: '#fff',
+			pointHighlightFill: '#fff',
+			pointHighlightStroke: 'rgba(151,187,205,1)',
+			data: [28, 48, 40, 19, 86, 27, 90],
 	}]
 };
 
 ReactDOM.render(
-  <Doughnut data={data} />,
+  <Line data={data} />,
   document.getElementById('root')
 );
