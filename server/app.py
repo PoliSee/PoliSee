037e00045bc2579fc3138a1483489a873bdd09bf
Chaitanya Sood <chaitanyasood01@gmail.com>
2018-11-30 19:19:56 -0600

    stuff kinda works

--- server/app.py
+++ server/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask import url_for
+from flask_cors import CORS
 import json
+from flask import jsonify
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def test():
     with open("../Tweets/tweetsentiment.json") as f:
+        '''
         data = f.read()
         f.close()
+        '''
+        data = jsonify(json.load(f))
         return data
-
